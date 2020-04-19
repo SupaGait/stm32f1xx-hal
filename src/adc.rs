1226bf1383b8442f68d4e8fd44fd775db9b2eb1c
Gerard K <n/a>
2020-04-19 15:59:49 +0200

    Added posibilty to use interrupts for ADC, specific ADC1 for now.

--- src/adc.rs
+++ src/adc.rs
@@ -706,3 +706,56 @@ where
         Transfer::w(buffer, self)
     }
 }
+
+impl Adc<ADC1> {
+    pub fn into_interrupt<PIN>(mut self, _pin: PIN) -> AdcInt<ADC1>
+    where
+        PIN: Channel<ADC1, ID = u8>,
+    {
+        // Enable interrupt after conversions.
+        self.rb.cr1.modify(|_, w| w
+            .eocie().set_bit()
+            .discen().clear_bit()
+        );
+        
+        // select channel
+        let channel = PIN::channel();
+        self.set_channel_sample_time(channel, self.sample_time);
+        self.rb.sqr3.modify(|_, w| unsafe { w.sq1().bits(channel) });
+
+        AdcInt::<ADC1> {
+            adc_info: self
+        }
+    }
+}
+
+pub struct AdcInt<ADC> {
+    adc_info: Adc<ADC>,
+}
+
+impl AdcInt<ADC1>{
+    pub fn enable(&mut self) {
+        self.adc_info.rb.cr2.modify(|_, w| w
+            .cont().set_bit()
+        );
+
+        // Separate the Start trigger.
+        self.adc_info.rb.cr2.modify(|_, w| w.
+            swstart().set_bit()
+        );
+    }
+    pub fn disable(&mut self) {
+        self.adc_info.rb.cr2.modify(|_, w| w.cont().clear_bit());
+    }
+    pub fn read_value(&self) -> u16 {
+        self.adc_info.rb.dr.read().data().bits()
+    }
+
+    pub fn is_ready(&self) -> bool {
+        self.adc_info.rb.sr.read().eoc().bit_is_set()
+    }
+
+    pub fn max_sample(&self) -> u16 {
+        self.adc_info.max_sample()
+    }
+}
